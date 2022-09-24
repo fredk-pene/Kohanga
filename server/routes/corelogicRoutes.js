7e447e846457277a84759823291f181147ef67ce
Mikesh Patel <mikeshpatel39@gmail.com>
2022-09-24 18:30:23 +1200

    second attempt to get the API working

--- server/routes/corelogicRoutes.js
+++ server/routes/corelogicRoutes.js
@@ -2,14 +2,20 @@ const request = require('superagent')
 const express = require('express')
 const router = express.Router()
 
-const key = process.env.access_token
+// const key = process.env.access_token
+// console.log(process.env)
 
 router.get('/', (req, res) => {
-  const addressURL = 'https://api-uat.corelogic.asia/sandbox/search/nz/matcher/address
+  const addressURL =
+    'https://api-uat.corelogic.asia/sandbox/search/nz/matcher/address?q=52%20Nikau%20Road%2C%20Otahuhu%2C%20Auckland'
   request
     .get(addressURL)
+    .set(
+      'authorization',
+      'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJDUlQiLCJNQVAiLCJQVFkiLCJTR1QiLCJTVFMiLCJUVEwiXSwicm9sZXMiOltdLCJpc3MiOiJodHRwczovL2FjY2Vzcy1hcGkuY29yZWxvZ2ljLmFzaWEiLCJlbnZfYWNjZXNzX3Jlc3RyaWN0Ijp0cnVlLCJleHAiOjE2NjQwMjYzNDEsImdlb19jb2RlcyI6WyJBQ1QgLSBGdWxsIFN0YXRlIiwiTlNXIC0gTWV0cm8iLCJOU1cgLSBSZWdpb25hbCIsIk5UIC0gRnVsbCBTdGF0ZSIsIlFMRCAtIE1ldHJvIiwiUUxEIC0gUmVnaW9uYWwiLCJTQSAtIE1ldHJvIiwiU0EgLSBSZWdpb25hbCIsIlRBUyAtIEZ1bGwgU3RhdGUiLCJWSUMgLSAoQUEpIEZ1bGwgU3RhdGUiLCJWSUMgLSBGdWxsIFN0YXRlIiwiVklDIC0gTWV0cm8iLCJWSUMgLSBSZWdpb25hbCIsIldBIC0gTWV0cm8iLCJXQSAtIFJlZ2lvbmFsIiwiTm9ydGggSXNsYW5kIiwiU291dGggSXNsYW5kIl0sImNsaWVudF9pZCI6IkpQQUJaU1dhZ1ZtQ1NoWkw1dEZJcWRLUDd2c2xjWXFGIiwic291cmNlX2V4Y2x1c2lvbiI6W119.kaDae9aKTc32CKOIdDrs6sSZAiRbtdTI6YLxJxKm0MThQql3yZRR1vFo62FnswudHloZSxB-et2ePqCLFn2turtRZQB5lsd7z6SjFGk5WHys4yJguDXdyWBh0i23iFdu-DKFPFgcS-0bg_po3lrNbuxFxcIdVHOfRvtmEFNxDkw'
+    )
     .then((response) => {
-      console.log(response.body)
+      console.log(response.body.matches[0].references.propertyIdList[0])
       res.json(response.body)
     })
     .catch((err) => console.error(err.message))
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const ownerSubRoute = require('./routes/ownerSubmission')
 const renterSubRoute = require('./routes/renterSubmission')
 const userRoutes = require('./routes/users')
-// const corelogicRoutes = require('./routes/corelogicRoutes')
+const corelogicRoutes = require('./routes/corelogicRoutes')
 const server = express()
 const request = require('superagent')
 const key = process.env.access_token
@@ -14,7 +14,7 @@ server.use(express.static(path.join(__dirname, 'public')))
 server.use('/api/v1/users', userRoutes)
 server.use('/api/v1/ownersubmissions', ownerSubRoute)
 server.use('/api/v1/rentersubmissions', renterSubRoute)
-// server.use('/api/v1/address', corelogicRoutes)
+server.use('/api/v1/address', corelogicRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'))
@@ -24,6 +24,7 @@ server.get('/address', (req, res) => {
   const addressURL = `https://api-uat.corelogic.asia/sandbox/search/nz/matcher/address?q=52%20Nikau%20Road%2C%20Otahuhu%2C%20Auckland" -H "accept: */*" -H "authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJDUlQiLCJNQVAiLCJQVFkiLCJTR1QiLCJTVFMiLCJUVEwiXSwicm9sZXMiOltdLCJpc3MiOiJodHRwczovL2FjY2Vzcy1hcGkuY29yZWxvZ2ljLmFzaWEiLCJlbnZfYWNjZXNzX3Jlc3RyaWN0Ijp0cnVlLCJleHAiOjE2NjQwMjY4OTgsImdlb19jb2RlcyI6WyJBQ1QgLSBGdWxsIFN0YXRlIiwiTlNXIC0gTWV0cm8iLCJOU1cgLSBSZWdpb25hbCIsIk5UIC0gRnVsbCBTdGF0ZSIsIlFMRCAtIE1ldHJvIiwiUUxEIC0gUmVnaW9uYWwiLCJTQSAtIE1ldHJvIiwiU0EgLSBSZWdpb25hbCIsIlRBUyAtIEZ1bGwgU3RhdGUiLCJWSUMgLSAoQUEpIEZ1bGwgU3RhdGUiLCJWSUMgLSBGdWxsIFN0YXRlIiwiVklDIC0gTWV0cm8iLCJWSUMgLSBSZWdpb25hbCIsIldBIC0gTWV0cm8iLCJXQSAtIFJlZ2lvbmFsIiwiTm9ydGggSXNsYW5kIiwiU291dGggSXNsYW5kIl0sImNsaWVudF9pZCI6IkpQQUJaU1dhZ1ZtQ1NoWkw1dEZJcWRLUDd2c2xjWXFGIiwic291cmNlX2V4Y2x1c2lvbiI6W119.UP2dDQYBlbdS9JltKXEVFzabG11ISf1dx-KM9NxRQe4XK7jzmLfRJkXe5gNmGpCPZPSvLnEMv_HY4l2ltFKvzfi9VtTxCpLFVb0qehzNhsvJ99IGGYRXZCSPh5zwS5LSTTcZWPF19RtRye3hyl5hNR6PRQ9RIjCkc8wYgIkkypE`
   request
     .get(addressURL)
+    .set('authorization', key)
     .then((response) => {
       console.log(response.body)
       res.json(response.body)
