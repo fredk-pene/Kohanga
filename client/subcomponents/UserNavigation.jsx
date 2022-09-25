b8cdcc8a1a5179bbb7d7ed8db8137ad1c9605f50
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-09-26 11:02:58 +1300

    initial commit

--- client/subcomponents/UserNavigation.jsx
+++ client/subcomponents/UserNavigation.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function UserNavigation() {
   return (
     <>
-      <p>User Navigation</p>
+      <div className="navigation-bar">
+        <Link className="back-to-search-btn" to="/">
+          <img
+            className="left-arrow"
+            alt="left arrow"
+            width="40px"
+            src="/server/public/assets/left-arrow.svg"
+          />
+          &nbsp; Back to Search
+        </Link>
+      </div>
     </>
   )
 }
