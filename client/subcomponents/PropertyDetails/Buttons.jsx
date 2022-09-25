16f54d4b9edd0c16f4ec1dad8c43b22a8ac419e9
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-09-26 11:37:49 +1300

    initial commit

--- client/subcomponents/PropertyDetails/Buttons.jsx
+++ client/subcomponents/PropertyDetails/Buttons.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Button() {
   return (
     <>
-      <p>Button</p>
+      <div className="report-challenge-btn-bar">
+        <div className="report-challenge-btn-bar">
+          <p className="back-to-search-btn">See Something thats not right?</p>
+          <Link to="/challenge" className="nav-submit-report-btn">
+            Challenge Report
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
