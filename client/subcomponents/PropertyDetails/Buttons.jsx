280bf0a1334493586dc8f032cd541bdfe7140732
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-09-26 16:24:07 +1300

    fixed the backend server routes

--- client/subcomponents/PropertyDetails/Buttons.jsx
+++ client/subcomponents/PropertyDetails/Buttons.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Button({ renterReports }) {
+export default function Button() {
   return (
     <>
       <div className="report-challenge-btn-bar">
