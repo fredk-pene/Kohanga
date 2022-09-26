1705a592ab2a9f7a2e51d01e9e4fd967e518ae8a
Karl Bayly <karlbayly@gmail.com>
2022-09-27 12:06:43 +1300

    thankyou component

--- client/views/SubmitThanktyou.jsx
+++ client/views/SubmitThanktyou.jsx
@@ -3,14 +3,20 @@ import React from 'react'
 import Footer from '../subcomponents/Footer'
 import Header from '../subcomponents/Header'
 
-function homePage() {
+function thankyou() {
   return (
     <>
       <Header />
-      <div className="page-width"></div>
+      <div className="page-width">
+        <div className="report-submit-container">
+          <div className="report-submit-header">
+            <h2 className="property-details-text">Report Submission</h2>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   )
 }
 
-export default homePage
+export default thankyou
