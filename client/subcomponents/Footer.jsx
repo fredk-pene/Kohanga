ee11eb00f67e0bdb4fdbb59ad75f50ff2804ea09
Karl Bayly <karlbayly@gmail.com>
2022-09-26 18:08:24 +1300

    footer fin and changes header

--- client/subcomponents/Footer.jsx
+++ client/subcomponents/Footer.jsx
@@ -3,35 +3,49 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    // <div className="footer">
-    <div className="w-full flex flex-wrap mx-3 border-black border">
-      <div className="flex flex-wrap mb-2">
-        <div className="">
-          <div className="w-full md:w-1/2 px-3 mb-4 pt-2">
-            <h1>
-              <Link to="/">Nested</Link>
-            </h1>
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-4 pt-2">
-            <ul>
-              <li>Contact</li>
-              <li>Level 5/12 Morgan St, Newmarket, Auckalnd New Zealand</li>
-              <li>INFO@KOHANGA.co.nz</li>
-            </ul>
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-4 pt-2">
-            <ul>
-              <li>Resources</li>
-              <li>How to Use</li>
-              <li>Product Newsz</li>
-              <li>Admin Login</li>
-              <li>Submit Review</li>
-              <li>Challenge Review</li>
-            </ul>
-          </div>
+    <div className="footer">
+      <div className="w-full flex flex-wrap mx-3 ">
+        {/* <div className="flex flex-wrap mb-2"> */}
+
+        {/* <div className="w-full md:w-1/2"> */}
+        <div className=" w-full md:w-1/4 px-3 mb-4 pt-2 align-baseline text-center  text-4xl uppercase pt-20">
+          {/* <h1 className=""> */}
+          <Link to="/">kōhanga</Link>
+          {/* </h1> */}
+        </div>
+
+        <div className="w=full md:w-1/4 px-3 mb-4 pt-2">
+          <ul>
+            <li>Contact</li>
+            <br />
+            <li>
+              <Link to="https://goo.gl/maps/3pAQ6rS8AvnEapZ18">
+                Level 5/12 Morgan St, Newmarket,
+                <br />
+                Auckalnd New Zealand
+              </Link>
+            </li>
+            <br />
+            <li>
+              <a href="mailto:info@kohanga.co.nz">INFO@KOHANGA.CO.NZ</a>{' '}
+            </li>
+          </ul>
+        </div>
+
+        <div className="w-full md:w-1/4 px-3 mb-4 pt-2">
+          <ul>
+            <li>Resources</li>
+            <br />
+            <li>How to Use</li>
+            <li>Product News</li>
+            <li>Admin Login</li>
+            <li>Submit Review</li>
+            <li>Challenge Review</li>
+          </ul>
         </div>
       </div>
-      {/* </div> */}
     </div>
+    // </div>
+    // </div>
   )
 }
--- client/views/Home.jsx
+++ client/views/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import video from '../../server/public/assets/Nest-bird.mp4'
 import AddressSearch from '../subcomponents/HomePage/AddressSearch'
-import Footer from '../subcomponents/Footer'
+// import Footer from '../subcomponents/Footer'
 // import SiteDescription from '../subcomponents/HomePage/SiteDescription'
 
 function homePage() {
@@ -14,7 +14,7 @@ function homePage() {
 
         <AddressSearch />
         {/* <SiteDescription /> */}
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </>
   )
