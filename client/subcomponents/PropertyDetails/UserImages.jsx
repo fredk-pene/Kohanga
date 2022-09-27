b0253506efe2d1140a9957c42d7a3f39724e2235
Karl Bayly <karlbayly@gmail.com>
2022-09-27 18:46:31 +1300

    images showing on seeded data porpoert pages

--- client/subcomponents/PropertyDetails/UserImages.jsx
+++ client/subcomponents/PropertyDetails/UserImages.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 
-export default function UserImages() {
+export default function UserImages({ report }) {
   return (
     <div className="full-report-column-container">
       <p className="small-title">User&apos;s Images</p>
       <div className="report-column-container property-details-secondary-text">
-        <img
-          src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg"
-          width="200"
-          alt="house"
-        />
-        <p>placeholder image</p>
+        <img src={(report = report.photo)} width="200" alt="house" />
+        <p>Photos</p>
       </div>
     </div>
   )
