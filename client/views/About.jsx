ec2997f75873d824c802a542cbd6b1d6fac6cb05
Karl Bayly <karlbayly@gmail.com>
2022-09-28 14:19:36 +1300

    added routes

--- client/views/About.jsx
+++ client/views/About.jsx
@@ -2,24 +2,28 @@ import React from 'react'
 
 import Footer from '../subcomponents/Footer'
 import Header from '../subcomponents/Header'
-import { useLocation, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function thankyou() {
-  const { state } = useLocation()
-
-  const address = state.address.replaceAll('-', ' ')
-
   return (
     <>
       <Header />
       <div className="page-width">
         <div className="report-submit-container">
           <div className="thankyou-report-response">
-            <h2>
-              Thank you for your report for {address}. It is now pending review.
-              Please take this oppertunity to submit another or just have a look
-              around.
-            </h2>
+            <h2>About</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
             <Link className="back-to-search-btn thankyou-page" to="/">
               <img
                 className="left-arrow"
