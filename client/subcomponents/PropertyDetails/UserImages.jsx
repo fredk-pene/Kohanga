b556f286c38604455181036162fa4d5fed254edb
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-09-28 14:53:47 +1300

    adjusted the user images

--- client/subcomponents/PropertyDetails/UserImages.jsx
+++ client/subcomponents/PropertyDetails/UserImages.jsx
@@ -5,8 +5,7 @@ export default function UserImages({ report }) {
     <div className="full-report-column-container">
       <p className="small-title">User&apos;s Images</p>
       <div className="report-column-container property-details-secondary-text">
-        <img src={(report = report.photo)} width="200" alt="house" />
-        <p>Photos</p>
+        <img src={(report = report.photo)} alt="house" />
       </div>
     </div>
   )
